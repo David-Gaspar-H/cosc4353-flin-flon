e98282c20a28ffbdd0bd0b8eaa47bdc4b72ea970
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2025-02-18 10:10:55 -0600

    Feat: added admin page component and corresponding route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import './App.css'
-import Login from './components/Login'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
-  return (
-    <>
-     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-     </Router>
-    </>
-  )
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+				</Routes>
+				<Routes>
+					<Route path="/admin" element={<Admin />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Admin.jsx
@@ -0,0 +1,11 @@
+import { Typography, Grid2 } from "@mui/material";
+
+const Admin = () => {
+	return (
+		<Grid2>
+			<Typography variant="h3">Admin</Typography>
+		</Grid2>
+	);
+};
+
+export default Admin;
