88c056a0577de4a5e4e5a8d409606659a2225c10
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2025-02-19 19:17:45 -0600

    Feat: added dashboard template and modified css files to correct issues with layout.

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,11 +1,106 @@
-import { Typography, Grid2 } from "@mui/material";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { createTheme } from "@mui/material/styles";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { useDemoRouter } from "@toolpad/core/internal";
 
-const Admin = () => {
+const NAVIGATION = [
+	{
+		segment: "dashboard",
+		title: "Dashboard",
+		icon: <DashboardIcon />,
+	},
+];
+
+const demoTheme = createTheme({
+	cssVariables: {
+		colorSchemeSelector: "data-toolpad-color-scheme",
+	},
+	colorSchemes: { light: true, dark: true },
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 600,
+			md: 600,
+			lg: 1200,
+			xl: 1536,
+		},
+	},
+});
+
+function DemoPageContent({ pathname }) {
 	return (
-		<Grid2>
-			<Typography variant="h3">Admin</Typography>
-		</Grid2>
+		<Box
+			sx={{
+				py: 4,
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				textAlign: "center",
+			}}
+		>
+			<Typography>Dashboard content for {pathname}</Typography>
+		</Box>
 	);
+}
+
+DemoPageContent.propTypes = {
+	pathname: PropTypes.string.isRequired,
 };
 
-export default Admin;
+function DashboardLayoutAccount(props) {
+	const { window } = props;
+
+	const [session, setSession] = React.useState({
+		user: {
+			name: "Bharat Kashyap",
+			email: "bharatkashyap@outlook.com",
+			image: "https://avatars.githubusercontent.com/u/19550456",
+		},
+	});
+
+	const authentication = React.useMemo(() => {
+		return {
+			signIn: () => {
+				setSession({
+					user: {
+						name: "Bharat Kashyap",
+						email: "bharatkashyap@outlook.com",
+						image: "https://avatars.githubusercontent.com/u/19550456",
+					},
+				});
+			},
+			signOut: () => {
+				setSession(null);
+			},
+		};
+	}, []);
+
+	const router = useDemoRouter("/dashboard");
+
+	// Remove this const when copying and pasting into your project.
+	const demoWindow = window !== undefined ? window() : undefined;
+
+	return (
+		// preview-start
+		<AppProvider
+			session={session}
+			authentication={authentication}
+			navigation={NAVIGATION}
+			router={router}
+			theme={demoTheme}
+			window={demoWindow}
+		>
+			<DashboardLayout>
+				<DemoPageContent pathname={router.pathname} />
+			</DashboardLayout>
+		</AppProvider>
+		// preview-end
+	);
+}
+
+export default DashboardLayoutAccount;
