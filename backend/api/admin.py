1af3b2fa9c7fb338a3ad7cc85ce6924040f98ef3
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2025-04-20 07:57:03 -0500

    Feat: register new Unit model to admin site page.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Unit
+
+
+class UnitAdmin(UserAdmin):
+    list_display = ("name", "parent")
+    search_fields = ("name",)
+    list_filter = ("parent",)
+
+
+admin.site.register(Unit, UnitAdmin)
+
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'role', 'status', 'is_staff')
-    list_filter = ('role', 'status', 'is_staff', 'is_superuser')
+    list_display = ("email", "username", "role", "status", "unit", "is_staff")
+    list_filter = ("role", "status", "unit", "is_staff", "is_superuser")
     fieldsets = UserAdmin.fieldsets + (
-        ('Custom Fields', {'fields': ('role', 'status')}),
+        ("Custom Fields", {"fields": ("role", "status", "unit")})
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Custom Fields', {'fields': ('role', 'status')}),
+        ("Custom Fields", {"fields": ("role", "status", "unit")})
     )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
